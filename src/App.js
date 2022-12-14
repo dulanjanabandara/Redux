9c6c808cdce024b43477c6c22d74db8c82cf6cf8
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-15 01:24:53 +0530

    Code cleaned up.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React from "react";
 import { createStore } from "redux";
 import reducer from "./reducer";
-import { DECREASE, INCREASE } from "./actions";
 
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 
 const initialStore = {
-  count: 0,
-  name: "john",
+  cart: cartItems,
+  total: 0,
+  amount: 0,
 };
 
 const store = createStore(reducer, initialStore);
-
-store.dispatch({ type: DECREASE });
-store.dispatch({ type: INCREASE });
-store.dispatch({ type: INCREASE });
-
-console.log(store.getState());
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar cart={store.getState()} />
+      <Navbar />
       <CartContainer cart={cartItems} />
     </main>
   );
