dbeee6a61a562a7f484c85fab16c4d8a403e55b6
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-15 05:00:44 +0530

    Implemented clear cart, increase, decrease and remove features of an item and the cart.

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,39 @@
-import { DECREASE, INCREASE } from "./actions";
+import { DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTAL, GET_AMOUNT } from "./actions";
 
 function reducer(state, action) {
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
+  }
+
+  if (action.type === DECREASE) {
+    let tempCart = [];
+
+    if (action.payload.amount === 1) {
+      tempCart = state.cart.filter((item) => item.id !== action.payload.id);
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      });
+    }
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === INCREASE) {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload.id) {
+        cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === REMOVE) {
+    return { ...state, cart: state.cart.filter((item) => item.id !== action.payload.id) };
+  }
   return state;
 }
 
